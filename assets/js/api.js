38e0c3e8db8719b3c32fa08ad4d2a41c62779952
Arthur Vinicius De Sena Moraes <arthur.vinigamer@gmail.com>
2025-08-31 23:03:45 -0300

    Atualizando os dados

---
+++ assets/js/api.js
@@ -0,0 +1,7 @@
+
+async function fetchProfileData() {
+    const url = 'https://raw.githubusercontent.com/lngg21/js-developer-portfolio/refs/heads/main/data/profile.json';
+    const response = await fetch(url)
+    const profileData = await response.json()
+    return profileData
+}
